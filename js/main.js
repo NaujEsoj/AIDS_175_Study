c9324af1d34c258ddb4a933130a4c739e30b3ffa
Juan Jose Garcia Zuluaga <juanjose.garcia@decameron.com>
2024-11-15 14:19:24 -0500

    added new NN version (16 inputs, time variable was removed)

--- js/main.js
+++ js/main.js
@@ -4,17 +4,29 @@ async function loadModel() {
 }
 
 async function makePrediction() {
+
     try {
         const model = await loadModel();
 
         // Collect form data
-        const inputs = [
-            'time', 'trt', 'age', 'wtkg', 'hemo', 'homo', 'drugs',
+        /* const inputs = [
+            'trt', 'age', 'wtkg', 'hemo', 'homo', 'drugs',
             'race', 'gender', 'str2', 'treat', 'offtrt', 'cd40',
             'cd420', 'cd80', 'cd820', 'karnof'
-        ].map(id => parseFloat(document.getElementById(id).value));
+        ].map(id => parseFloat(document.getElementById(id).value)); */
+
+        inputs = [1.000000, 	0.844828, 	0.143032, 	0.0, 	0.0, 	0.0, 	0.0, 	0.0, 	1.0, 	1.0, 	0.0, 	0.135113, 	0.157944, 	0.070811, 	0.074437, 	0.666667]
+
+        function normalizeData(inputs) {
+            const min = Math.min(...inputs);
+            const max = Math.max(...inputs);
+            return inputs.map(value => (value - min) / (max - min));
+        }
 
         // Convert inputs to tensor and make prediction
+        const normalizedInputs = normalizeData(inputs);
+        console.log(normalizedInputs);
+        //const inputTensor = tf.tensor([normalizedInputs]);
         const inputTensor = tf.tensor([inputs]);
         const prediction = model.predict(inputTensor);
         const probabilities = await prediction.data();
@@ -42,5 +54,6 @@ async function makePrediction() {
                 Error making prediction. Please check your inputs and try again.
             </div>
         `;
+        console.log(normalizedInputs);
     }
 }
